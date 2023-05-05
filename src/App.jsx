f29ad38e5266b663a12f6a3db8de9ae02644fad6
TQY <qingyuantay@gmail.com>
2023-05-05 17:15:46 +0800

    Build grid for site

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-export function App(){
-    return <div>Hello world</div>
+import s from "./style.module.css";
+export function App() {
+  return (
+    <div className={s.main_container}>
+
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text"></input>
+          </div>
+        </div>
+      </div>
+
+      <div className={s.tv_show_detail}>TV Show Detail</div>
+
+      <div className={s.recommended_tv_shows}>Recommended TV Shows</div>
+      
+    </div>
+  );
 }
