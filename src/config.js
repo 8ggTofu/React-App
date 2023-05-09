c41a6ce7ffed81dc37f4a660fbaaa619e3c4f41c
TQY <qingyuantay@gmail.com>
2023-05-09 23:14:09 +0800

    Add TV show details

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/"
+const API_KEY_PARAM = "0a7d745f1df73a81968f42baec72c7cf"
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL } 
