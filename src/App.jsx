2660733f2d3f439291478982261f516c9b060848
TQY <qingyuantay@gmail.com>
2023-05-12 14:45:49 +0800

    create Logo, title and subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { TVShowAPI } from "./api/tv-show";
 import s from "./style.module.css";
 import { BACKDROP_BASE_URL } from "./config";
 import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import logoImg from "./assets/images/logo.png";
+import { Logo } from "./components/Logo/Logo";
 
 export function App() {
   const [currentTVShow, setCurrentTVShow] = useState();
@@ -31,8 +33,13 @@ export function App() {
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
-            <div>LOGO</div>
-            <div>Subtitle</div>
+            <div>
+              <Logo
+                img={logoImg}
+                title="Watowatch"
+                subtitle="Find a show you may like"
+              />
+            </div>
           </div>
 
           <div className="col-md-12 col-lg-4">
--- src/components/TVShowDetail/TVShowDetail.jsx
+++ src/components/TVShowDetail/TVShowDetail.jsx
@@ -1,4 +1,4 @@
-import { FiveStarRating } from "../../FiveStarRating/FiveStarRating";
+import { FiveStarRating } from "../FiveStarRating/FiveStarRating";
 import s from "./style.module.css";
 
 export function TVShowDetail({ tvShow }) {
