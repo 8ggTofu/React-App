a9871a34f2bb85a927221538883f73873968e29f
TQY <qingyuantay@gmail.com>
2023-05-15 21:31:35 +0800

    searchbar - design

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+import s from "./style.module.css"
+import {Search as SearchIcon } from "react-bootstrap-icons"
+export function SearchBar (props) {
+    return <>
+    <SearchIcon size={27} className={s.icon}/>
+    <input className={s.input} type="text" placeholder="Search for a TV show you may like" />
+    </>
+}
